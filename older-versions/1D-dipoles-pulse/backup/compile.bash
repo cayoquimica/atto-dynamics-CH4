b2ff65176a5e816ccf61f0c319f987923bb6e78b
cayoquimica <cayoquimica@gmail.com>
2019-11-06 11:43:50 +0100

    Putting the folder older-versions to a 7zip file. Removing all executables and temporary files created in a compilation

--- older-versions/1D-dipoles-pulse/backup/compile.bash
+++
@@ -1,4 +0,0 @@
-ifort -c global_param.f90
-ifort -c dyn.f90
-ifort global_param.o dyn.o -o dyn.exe
-./dyn.exe
--- older-versions/1D-dipoles-pulse/backup/g-compile.bash
+++
@@ -1,4 +0,0 @@
-gfortran-8 -c global_param.f90
-gfortran-8 -c dyn.f90
-gfortran-8 global_param.o dyn.o -o g-dyn.exe
-./g-dyn.exe
